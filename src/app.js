4d9df859afedc80c8a4aebe33d28f292d944c702
IgnExotiic <IgnExoticJunior@gmail.com>
2023-08-31 20:07:53 -0700

    1.4.1

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ DiscordRPC.register(clientId);
 async function setActivity() {
    if (!RPC) return;
    RPC.setActivity({
-       state: `Jugando a "Squidgames 2"`,
+       state: `Jugando a "The Backrooms"`,
        startTimestamp: Date.now(),
        largeImageKey: 'asteroid',
        largeImageText: `Asteroid Studios`,
